8d3e8c598bc8e1a8586211d7578c9fd1d37a2cc3
imac <alex.cheng@dingtone.me>
2019-05-30 16:29:46 +0800

    测试

---
+++ Example/Pods/Target Support Files/Pods-TestCategory_Tests/Pods-TestCategory_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestCategory_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TestCategory_Tests
+@end
---
+++ Example/Pods/Target Support Files/TestCategory/TestCategory-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestCategory : NSObject
+@end
+@implementation PodsDummy_TestCategory
+@end
